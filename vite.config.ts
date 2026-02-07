68eb39a1f148cb9ec556c05baeb189379fbf53ba
Koki Oyatsu <kaishuu0123@gmail.com>
2026-02-07 18:50:09 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./", // 相対パスにてサブディレクトリ対応
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 3000
-  }
-})
+    host: "0.0.0.0",
+    port: 3000,
+  },
+});
